316ef5070a948199b5297d6a35213df09d76bfdc
Apoorva <apoorvask18@gmail.com>
2023-09-25 13:59:41 +0530

    arrays

---
+++ arrays/arrays.js
@@ -0,0 +1,17 @@
+// let arr = [];
+// console.log(typeof(arr));
+
+// let arr2 = new Array();
+// console.log(typeof(arr2));
+
+// let arr3 = [ 'Apple', { name: 'John' }, true, function() { alert('hello'); } ];
+// console.log(arr3[1]);
+// console.log(arr3[1].name);
+
+let matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+console.log(matrix);
