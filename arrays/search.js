0ef915ac1d310ab44ec8d521b6fb327986a300cb
Apoorva <apoorvask18@gmail.com>
2023-09-26 12:28:15 +0530

    arrays filter

--- arrays/search.js
+++ arrays/search.js
@@ -1,9 +1,18 @@
 let user = [
     {id:1 , name:"apoorva"},
     {id:2 , name:"ben"},
-    {id:3 , name:"jay"}
+    {id:3 , name:"jay"},
+    {id:1 , name:"suzy"},
 ];
 
-let u = user.find(item => item.id == 1);
+// let u = user.find(item => item.id == 1);
+// console.log(u.name);
 
-console.log(u.name);
+// let usr = user.filter(item => item.id == 1);
+// console.log(usr.name);
+
+let usr = user.filter(item => {
+    if(item.id == 1){
+    console.log(item.name);
+    }
+});
