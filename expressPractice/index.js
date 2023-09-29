e4958e01ff60851332a45c5ca697446128d818d7
Apoorva <apoorvask18@gmail.com>
2023-09-29 10:58:42 +0530

    middleware

--- expressPractice/index.js
+++ expressPractice/index.js
@@ -7,6 +7,10 @@ const PORT = 3001;
 app.listen(PORT, () => console.log(`Running server on port ${PORT}!`));
 
 app.use(express.json()); //enable middleware for post content to be parsed
+app.use((req,res,next) => {                      //middleware function, its mandatory to call next() so that next middleware function gets called
+    console.log(`${req.method} : ${req.url}`);
+    next();
+})
 
 let studyItems = [{
     topic: "js",
