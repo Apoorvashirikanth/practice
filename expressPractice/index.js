3d1511fb081f0fad2455ed2cb2e46e5de8b843a1
Apoorva <apoorvask18@gmail.com>
2023-09-29 09:54:59 +0530

    express post

--- expressPractice/index.js
+++ expressPractice/index.js
@@ -6,6 +6,22 @@ const PORT = 3001;
 
 app.listen(PORT, () => console.log(`Running server on port ${PORT}!`));
 
+app.use(express.json()); //enable middleware for post content to be parsed
+
+let studyItems = [{
+    topic: "js",
+    hours: 30,
+},
+{
+    topic: "html",
+    hours: 20,
+},
+{
+    topic: "css",
+    hours: 5,
+},
+];
+
 app.get('/study', (req, res) => {
     // res.send("apoorva"); //sending string
     // res.send({name: "ap" , id: 1}); //sending obj
@@ -13,17 +29,11 @@ app.get('/study', (req, res) => {
     // res.send(['g','f']) //sending arr of strings
     //sending arr of obj
     //can only send once
-    res.send([{
-        topic: "js",
-        hours: 30,
-    },
-    {
-        topic: "html",
-        hours: 20,
-    },
-    {
-        topic: "css",
-        hours: 10,
-    },
-])
+    res.send(studyItems);
 });
+
+app.post('/study', (req,res) => {
+    console.log(req.body);
+    studyItems.push(req.body);
+    res.send(201);
+})
