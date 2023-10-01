4c1353349c83263296decbe6c25eab8f8709951f
Apoorva <apoorvask18@gmail.com>
2023-10-01 16:12:26 +0530

    query parameters

--- expressPractice/routes/subjectRoutes.js
+++ expressPractice/routes/subjectRoutes.js
@@ -4,20 +4,31 @@ const router = Router();
 let subjectList = [
     {
         subject: "Science",
-        grade: 10
+        grade: 10,
     },
     {
         subject: "Math",
-        grade: 11
+        grade: 11,
     },
     {
         subject: "social",
-        grade: 9
+        grade: 9,
     }
 ];
 
-router.get('/subjects', (req,res) => {
-    res.send(subjectList);
+// router.get('/subjects', (req,res) => {
+//     res.send(subjectList);
+// });
+
+router.get('/subjects',(req,res) => { //query params
+    // console.log(req.query);
+    const { grade } = req.query;
+    const parsedG = parseInt(grade);
+    if(!isNaN(parsedG)){
+        const filterdG = subjectList.filter((s) => s.grade == parsedG);
+        res.send(filterdG);
+    }
+    else res.send(subjectList);
 });
 
 router.post('/subjects', (req,res) => {
