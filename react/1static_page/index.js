992605a50eb6543a8a25380c08463ec3eda5cfcf
Apoorva <apoorvask18@gmail.com>
2023-10-04 16:13:37 +0530

    react

--- react/1static_page/index.js
+++ react/1static_page/index.js
@@ -1,17 +1,65 @@
-const page = (
-    <div>   
-    <img src="./react-logo.png" width="40px"></img>
-    <h1> Fun facts about react </h1>
+// const page = (
+//     <div>   
+//     <img src="./react-logo.png" width="40px"></img>
+//     <h1> Fun facts about react </h1>
+//     <ul>
+//         <li>It is declarative</li>
+//         <li>composable</li>
+//         <li>hirable skill</li>
+//         <li>manged by facebook</li>
+//     </ul>
+//     </div>
+// );
 
-    <ul>
-        <li>Itis declarative</li>
-        <li>composable</li>
-        <li>hirable skill</li>
-        <li>manged by facebook</li>
+// ReactDOM.render(page, document.getElementById("root"));
 
-    </ul>
+// custom components
 
+function Header(){
+     return(
+        <header>
+            <nav className = "nav">
+                <img src = "./react-logo.png" className="nav-logo" />
+                <ul className = "nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+
+    )
+}
+
+function Footer(){
+    return(
+        <footer className="nav-footer">
+             <small>2023 apoorva development. All rights reserved</small>
+        </footer>
+    )
+}
+
+function MainComponent(){
+    return(
+        <div>
+            <h1>reasons why i like react</h1>
+        <ol>
+            <li>react is cool</li>
+            <li>its composable</li>
+            <li>I love react</li>
+        </ol>
+        </div>
+    )
+}
+
+function MyFirstComponent(){    //parent component
+    // children compnents 
+    return(
+    <div>
+        <Header />            
+        <MainComponent />
+        <Footer />
     </div>
-);
+)};
 
-ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(<MyFirstComponent />, document.getElementById("root"));
