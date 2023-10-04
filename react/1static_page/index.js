7e07ab45a6e8e4fd25019a42feffd2bcdc59b965
Apoorva <apoorvask18@gmail.com>
2023-10-04 11:47:50 +0530

    react basics

---
+++ react/1static_page/index.js
@@ -0,0 +1,17 @@
+const page = (
+    <div>   
+    <img src="./react-logo.png" width="40px"></img>
+    <h1> Fun facts about react </h1>
+
+    <ul>
+        <li>Itis declarative</li>
+        <li>composable</li>
+        <li>hirable skill</li>
+        <li>manged by facebook</li>
+
+    </ul>
+
+    </div>
+);
+
+ReactDOM.render(page, document.getElementById("root"));
